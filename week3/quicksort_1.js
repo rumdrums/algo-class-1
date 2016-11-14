8ac37378ccaa3d644913ed50c74daf50f97a2b58
James Lucas <jamesmlucas@gmail.com>
2016-11-14 02:02:37 -0600

    not working

--- week3/quicksort_1.js
+++ week3/quicksort_1.js
@@ -2,18 +2,55 @@ var fs = require('fs');
 
 var total = 0;
 
-var partition = function(A, l, r) {
-  // console.log('partition called: ', A, l, r);	
-  return partitionHigh(A, l, r);
+var getMiddle = function(A, l, r) {
+  // return index of middle element
+  var idx = Math.floor((r-l)/2);
+  return idx;	
 };
 
-var partitionLow = function(A, l, r) {
+
+var getMedian = function(A) {
+  A.sort(function(a,b) {
+    return a - b;
+  });
+  // console.log('median: ', A, A[getMiddle(A, 0, A.length-1)]);
+  return A[getMiddle(A, 0, A.length-1)];
+};
+
+
+var partition = function(A, l, r) {
+  console.log('called partition: ', A.slice(l, r+1));
+  var mid = getMiddle(A, l, r);
+  var med = getMedian([A[l], A[mid], A[r]]);
+  var medianIdx = A.indexOf(med);
+  return partitionRight(A, l, r, medianIdx); 
+
+  // this is bad: assumes there are no duplicates,
+  // and is also just dumb:
+  switch(medianIdx) {
+  	case l:
+  	  // console.log('I called partitionLeft', A, l, r, p, A[mid]);
+  	  return partitionLeft(A, l, r);
+  	case mid:
+   	  console.log('I called partitionMedian');
+  	  return partitionMedian(A, l, r, medianIdx);
+  	case r:
+  	  // console.log('I called partitionRight', A, l, r, p, A[mid]);  	  
+  	  return partitionRight(A, l, r);
+  	default:
+  	  console.log('I fucked up', A, l, r, p, A[mid]);
+  }
+};
+
+
+var partitionLeft = function(A, l, r) {
   total += r - l;	
   p = A[l];
+  console.log('pivot: ', p);
   var i = l + 1;
   for(var j=l+1; j <= r; j++) {
   	if (A[j] < p) {
-  	  // console.log('swapping ' + A[i] + ' and ' + A[j]);	
+  	  // console.log('swapping ' + A[j] + ' and ' + A[i]);	
   	  var temp = A[i];
   	  A[i] = A[j];
       A[j] = temp;
@@ -24,54 +61,62 @@ var partitionLow = function(A, l, r) {
   var temp = A[l];
   A[l] = A[i-1];
   A[i-1] = temp;
+  console.log('returning: ', i-1);
   return i-1;
 };
 
-var partitionHigh = function(A, l, r) {
+
+var partitionRight = function(A, l, r) {
   total += r - l;	
   p = A[r];
+  console.log('pivot: ', p);
   var i = l;
-  for(var j=l; j < r; j++) {
+  for(var j=l; j <= r; j++) {
   	if (A[j] < p) {
-   	  // console.log('swapping ' + A[i] + ' and ' + A[j]);
+   	  console.log('swapping ' + A[i] + ' and ' + A[j]);
   	  var temp = A[i];
   	  A[i] = A[j];
       A[j] = temp;
       i++;
   	}
   }
-  // console.log('swapping ' + A[r] + ' and ' + A[i]);
+  console.log('finally, swapping ' + A[r] + ' and ' + A[i]);
   var temp = A[r];
   A[r] = A[i];
   A[i] = temp;
-  return i-1;
+  console.log('returning: ', Math.max(i-1,0));
+  return Math.max(i-1,0);
 };
 
-var partitionMedian = function(A, l, r) {
+
+var partitionMedian = function(A, l, r, medianIdx) {
   total += r - l;	
-  p = A[r];
   var i = l;
-  for(var j=l; j < r; j++) {
+  var p = A[medianIdx];
+  // console.log('A, l, r, medianIdx, p: ', A, l, r, medianIdx, p);
+  for(var j=l; j <= r; j++) {
+  	// this is bad: assumes there are no duplicates:
+  	if (A[j] == p) continue;
   	if (A[j] < p) {
    	  // console.log('swapping ' + A[i] + ' and ' + A[j]);
   	  var temp = A[i];
   	  A[i] = A[j];
       A[j] = temp;
       i++;
+      // this is bad: assumes there are no duplicates:
+      if (A[i] == p) i++;
   	}
   }
-  // console.log('swapping ' + A[r] + ' and ' + A[i]);
-  var temp = A[r];
-  A[r] = A[i];
-  A[i] = temp;
+  console.log('returning: ', i-1);
   return i-1;
 };
+
 
 var quickSort = function(A, l, r) {
-  // console.log('quicksort called: ', A, l, r);	
-  if ((r-l) < 1) {
+  if ((r-l) < 2) {
   	return;
   }
+  console.log('quicksort called: ', A.slice(l, r+1));	
   p = partition(A, l, r);
   quickSort(A, l, p);
   quickSort(A, p+1, r);
@@ -79,10 +124,11 @@ var quickSort = function(A, l, r) {
 
 
 var main = function() {
-
-  fs.readFile('QuickSort.txt', function(err, data) {
+  myFile = 'QuickSort.txt';
+  myFile = 'test1.txt'
+  fs.readFile(myFile, function(err, data) {
     if (err) throw err;
-    var A = data.toString().split('\r\n');
+    var A = data.toString().split('\n');
     for(var i=0; i<A.length; i++) {
     	if (A[i] == '') A.splice(i, 1);
     	else A[i] = parseInt(A[i]);
@@ -93,11 +139,11 @@ var main = function() {
 	console.log('total: ', total);
   });
 
-  /*
-  var A = [2,4,1,3];
-  quickSort(A, 0, A.length-1);
-  console.log(A);
-  */
+  
+  // var A = [2,4,1,3];
+  // quickSort(A, 0, A.length-1);
+  // console.log(A);
+  
 };
 
 
